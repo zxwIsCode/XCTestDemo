35fa383de990c306c35ca44349584d8d93eeaef5
zxwIsHot <1824496534@qq.com>
2017-06-21 17:32:24 +0800

    更改项目文件目录并添加部分测试注释

--- XCTestDemoUITests/XCTestDemoUITests.m
+++ XCTestDemoUITests/XCTestDemoUITests.m
@@ -35,6 +35,7 @@
 
 }
 
+// 执行一次的界面跳转
 - (void)testCode {
     XCUIApplication *app = [[XCUIApplication alloc] init];
     
@@ -44,6 +45,15 @@
     
 }
 
+/*
+ XCTest 测试用例使用注意事项：
+ 
+ 1.更改项目文件名称以及部分UI控件如（button上面的文字时就要重新创建用例）
+ 2.全程测试用例最好用英文表示，如button上的文字为中文时会报错！
+ 3.还有创建的测试用例文件为了要在测试方法（testExample）中，需要把当前编辑器的光标放在该方法中，再点击小红圈来测试！
+ */
+
+// 执行多次的界面跳转
 - (void)testExample {
     NSLog(@"UI测试中");
     
